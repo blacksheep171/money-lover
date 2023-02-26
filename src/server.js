5c90c52b456f5a643f2dac90163c85ab32d40598
Hieu <ngxuanhieu97@gmail.com>
2023-02-26 15:33:51 +0700

    init source

---
+++ src/server.js
@@ -0,0 +1,23 @@
+// required library
+import express from 'express';
+import configViewEngine from './configs/viewEngine';
+require('dotenv').config();
+// create instance
+const app = express()
+const port = process.env.PORT
+
+// console.log(">> check port: ", port);
+
+// console.log(process.env); 
+configViewEngine(app);
+app.get('/', (req, res) => {
+  res.render('test/index.ejs')
+})
+
+app.get('/home', (req, res) => {
+    res.send(`Welcome Home!`)
+})
+  
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
